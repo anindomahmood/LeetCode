783495e5b36aff20734cbbe7f58a0beef3875684
anindomahmood <anindomahmood1@gmail.com>
2026-06-30 03:07:07 +0600

    Initial commit

---
+++ solver.js
@@ -0,0 +1,12 @@
+// Question 1: Write a function createHelloWorld. It should return a new function that always returns "Hello World". (easy)
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function() {
+        return "Hello World";
+    };
+};
+
+const f = createHelloWorld();
+console.log(f());
